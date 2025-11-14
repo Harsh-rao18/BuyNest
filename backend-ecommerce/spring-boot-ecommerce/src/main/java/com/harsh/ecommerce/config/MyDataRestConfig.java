bcfe91c9ddb540af07306ad432b6e03e82f8d1a4
Harsh-rao18 <harshrao64644@gmail.com>
2025-11-14 20:58:11 +0530

    Add Product & ProductCategory entities with repositories and Spring Data REST

---
+++ backend-ecommerce/spring-boot-ecommerce/src/main/java/com/harsh/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,25 @@
+package com.harsh.ecommerce.config;
+
+import com.harsh.ecommerce.entity.Product;
+import com.harsh.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.DELETE,HttpMethod.PUT,HttpMethod.POST};
+
+        // disable Http methods for Product
+        config.getExposureConfiguration().forDomainType(Product.class).withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))).withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions) ));
+        // disable Http methods for ProductCategory
+        config.getExposureConfiguration().forDomainType(ProductCategory.class).withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))).withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions) ));
+    }
+}
